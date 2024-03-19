18ce95698a4c3c409b73dded15dfdc8008367ab2
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-03-19 09:45:27 +0800

    updated code

--- gui/scripts/compat.js
+++ gui/scripts/compat.js
@@ -1,26 +1,37 @@
 /* compat.js
 You shouldn't be opening the HTMLs elsewhere. */
 
-let browser_data = {};
-export function refresh() {
+// Import modules.
+import texts from "./read.JS";
+
+// Initialize browser data.
+let browser_data = {};
+
+function refresh() {
   /* Update variable containing the browser data.
 
-  Returns: (dict) the updated browser data	*/
-  browser_data['URL'] = window.location.href;
-  return (browser_data['URL']);
-};
+  Returns: (dict) the updated browser data
+  */
 
-export function restrict() {
-  /* Restrict pages containing this function from being loaded outside the extension.
+  browser_data.URL = window.location.href;
+  return (browser_data.URL);
+}
 
-  Returns: (bool) page open is correct	*/
+export default class compat {
+  static restrict() {
+    /* Restrict pages containing this function from being loaded outside the extension.
 
-  refresh();
-  const browser_extension_URL_contents = new RegExp(`^(?!file:\/\/|http:\/\/|https:\/\/)(.*-extension:\/\/.*)`);
-  const browser_condition_met = browser_extension_URL_contents.test(browser_data.URL);
-  if (!browser_condition_met) {
-    alert("This page can not be opened. \r此网页无法打开。");
-  };
-  console.log(browser_data.URL);
-  return (browser_condition_met);
-};
+    Returns: (bool) page open is correct
+    */
+
+    refresh();
+
+    const browser_extension_URL_contents = new RegExp(`^(?!file:\/\/|http:\/\/|https:\/\/)(.*-extension:\/\/.*)`);
+    const browser_condition_met = browser_extension_URL_contents.test(browser_data.URL);
+    if (!browser_condition_met) {
+      alert(texts.universal(`invalid_open`));
+    }
+
+    return (browser_condition_met);
+  }
+}
