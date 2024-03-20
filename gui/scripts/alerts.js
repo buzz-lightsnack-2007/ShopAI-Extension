716d4ac3e5defd04e00bfc8d10a9a27596a1611a
buzzcode2007 <73412182+buzz_lightsnack_2007@users.noreply.github.com>
2024-03-20 23:32:55 +0800

    create alerts interface

---
+++ gui/scripts/alerts.js
@@ -0,0 +1,18 @@
+/* alerts.js
+Alert management system.
+*/
+
+export function confirm_action() {
+    let user_response = false; 
+
+    (async () => {
+        // Import the module. 
+        let reader = await import(chrome.runtime.getURL("gui/scripts/read.JS"));
+
+        // Get the user response. 
+        user_response = confirm(reader.read("localized", `GUI_alert_confirm_action_text`));
+
+    })();
+    // Return the user response. 
+    return (user_response);
+};
