8f0f7fb9237343843193ffb4bf5f762e55905592
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-03-25 12:39:38 +0800

    major moving around of files

--- gui/scripts/popup.js
+++ gui/scripts/popup.js
@@ -2,52 +2,23 @@
   Build the interface for popup
 */
 
-// Import modules. 
+// Import modules.
 import texts from './read.JS';
 import windowman from './windowman.JS';
+let secretariat = await import(chrome.runtime.getURL("scripts/secretariat.js"));
 
 
-     // Import modules.
-     let secretariat = await import(chrome.runtime.getURL("scripts/secretariat.js"));
-     
-     let pref_pane = 0;
+function start() {
+	windowman.prepare();
+}
+/* Populate the strings on the page. */
+function say(element) {
+	// document.title
+}
 
+function main() {
+	let tab = start();
+	say(tab);
+}
 
-     function start() {
-          windowman.prepare();
-
-          return(new windowman(`body`, null, null, {'Close': true}));
-     }
-
-     /* Add the UI design. */
-     function design(element) {
-          // Set the event of the window. 
-          function controls() {
-               document.getElementById(element[`titlebar`][`controls`][`Close`][`ID`]).onclick = function(){element.terminate(false)};
-          }
-
-
-          
-
-          // TODO work on the content; perhaps, it must read from JSON of valid prefs
-
-          controls();
-
-     }
-
-     /* Populate the strings on the page. */
-     function say(element) {
-          element.update(element[`titlebar`][`title`], texts.localized(`GUI_title_preferences`));
-
-     };
-
-     function main() {
-          let tab = start();
-          design(tab);
-          say(tab);
-     }
-
-     main();
-
-
-
+main();
