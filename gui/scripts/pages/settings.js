8164ccf8e4bb4c00169f5dd80bb70aa9f3282d8d
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-04-02 19:58:45 +0800

    update reference to be as default functions

--- gui/scripts/pages/settings.js
+++ gui/scripts/pages/settings.js
@@ -6,14 +6,14 @@
 //import { windowman } from "../windowman.js";
 
 async function build() {
-	let secretariat = (
-		await import(chrome.runtime.getURL("scripts/secretariat.js"))
-	);
-	let windowman = (
-		await import(chrome.runtime.getURL("gui/scripts/windowman.js"))
-	).default;
+  let secretariat = await import(
+    chrome.runtime.getURL("scripts/secretariat.js")
+  );
+  let windowman = (
+    await import(chrome.runtime.getURL("gui/scripts/windowman.js"))
+  ).default;
 
-	let window = new windowman();
+  let window = new windowman();
 }
 
 /*
@@ -47,51 +47,61 @@ function main() {
 		Define the mapping of each button.
 		*/
 function events() {
-	if (document.querySelector(`[data-action="filters,update"]`)) {
-		document.querySelector(`[data-action="filters,update"]`).addEventListener(`click`, async () => {
-			let filters = new ((
-				await import(chrome.runtime.getURL(`scripts/filters.js`))
-			).default);
-			await filters.update();
-		});
-	}
-	
-	if (document.querySelector(`[data-action="filters,add,one"]`)) {
-		document.querySelector(`[data-action="filters,add,one"]`).addEventListener(`click`, async () => {
-			(async () => {
-				// Import the filters module.
-				let texts = (await import(chrome.runtime.getURL(`gui/scripts/read.js`))).default;
-				let filters = new ((await import(chrome.runtime.getURL(`scripts/filters.js`))).default);
+  if (document.querySelector(`[data-action="filters,update"]`)) {
+    document
+      .querySelector(`[data-action="filters,update"]`)
+      .addEventListener(`click`, async () => {
+        let filters = new (
+          await import(chrome.runtime.getURL(`scripts/filters.js`))
+        ).default();
+        await filters.update();
+      });
+  }
 
-				// Open text input window for adding a filter.
-				let filter_source = prompt(texts.localized(`settings_filters_add_prompt`));
-				if (filter_source.trim) {
-					filters.update(filter_source.trim);
-				};
-			})();
-		});
-	}
+  if (document.querySelector(`[data-action="filters,add,one"]`)) {
+    document
+      .querySelector(`[data-action="filters,add,one"]`)
+      .addEventListener(`click`, async () => {
+        (async () => {
+          // Import the filters module.
+          let texts = (
+            await import(chrome.runtime.getURL(`gui/scripts/read.js`))
+          ).default;
+          let filters = new (
+            await import(chrome.runtime.getURL(`scripts/filters.js`))
+          ).default();
 
-	if (document.querySelector(`[data-action="storage,clear"]`)) {
-		document
-			.querySelector(`[data-action="storage,clear"]`)
-			.addEventListener(`click`, async () => {
-				let storage = (
-					await import(chrome.runtime.getURL(`scripts/secretariat.js`))
-				)["secretariat"];
-				storage.forget(`sites`);
-			});
-	}
+          // Open text input window for adding a filter.
+          let filter_source = prompt(
+            texts.localized(`settings_filters_add_prompt`),
+          );
+          if (filter_source ? filter_source.trim() : false) {
+            filters.update(filter_source.trim());
+          }
+        })();
+      });
+  }
+
+  if (document.querySelector(`[data-action="storage,clear"]`)) {
+    document
+      .querySelector(`[data-action="storage,clear"]`)
+      .addEventListener(`click`, async () => {
+        let storage = (
+          await import(chrome.runtime.getURL(`scripts/secretariat.js`))
+        )["secretariat"];
+        storage.forget(`sites`);
+      });
+  }
 }
 
 //main();
 function load() {
-	document.addEventListener("DOMContentLoaded", function () {
-		M.AutoInit();
-		events();
-	});
+  document.addEventListener("DOMContentLoaded", function () {
+    M.AutoInit();
+    events();
+  });
 
-	build();
+  build();
 }
 
 load();
