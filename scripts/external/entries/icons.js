a549cbe76aa4884ed12285560fa0d03c9fa8ff50
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-04-24 16:22:49 +0800

    add icon management

---
+++ scripts/external/entries/icons.js
@@ -0,0 +1,15 @@
+import BrowserIcon from '/scripts/GUI/browser.icon.js';
+import Image from '/scripts/strings/image.js';
+
+class IconIndicator {
+     static async enable() {
+          // WIP. Make sure to determine if the product is bad / good
+          BrowserIcon.set(await Image.get('default'));
+     }
+
+     static async disable() {
+          BrowserIcon.set(await Image.get('disabled'));
+     }
+}
+
+export {IconIndicator as default};
