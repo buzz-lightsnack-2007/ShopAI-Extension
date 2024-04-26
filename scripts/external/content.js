2372e09c6d8c39d4c9aa6a5ae12202647d131ef1
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-04-26 22:54:54 +0800

    now make the new content script executable

--- scripts/external/content.js
+++ scripts/external/content.js
@@ -7,8 +7,8 @@ The content script
 // Import the necessary modules.
 (async () => {
      // Import the watchman module.
-     let watchman = await import(chrome.runtime.getURL("scripts/external/watch.js"));
-     
+     let watchman = (await import(chrome.runtime.getURL("scripts/external/watch.js"))).default;
+
      // Begin the job.
      watchman.job();
-})
+})()
