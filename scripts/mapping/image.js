1aa887cab1183be38c44c9a14e1ba174a23e621a
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-04-27 23:49:05 +0800

    add local URL to local images

--- scripts/mapping/image.js
+++ scripts/mapping/image.js
@@ -12,6 +12,20 @@ class Image {
 			.then((jsonData) => {
 				let image = {'raw': jsonData[name]};
 				image[`filtered`] = (image[`raw`] && size) ? image[`raw`][String(size)] : image[`raw`];
+
+				// Set the appropriate URL. 
+				if (typeof image[`filtered`] == `string` && !image[`filtered`].includes(`://`)) {
+					image[`filtered`] = chrome.runtime.getURL(image[`filtered`]);
+				} else if (((typeof image[`filtered`]).includes(`obj`) && image[`filtered`] != null && !Array.isArray(image[`filtered`])) ? (Object.keys(image[`filtered`]).length) : false) {
+					Object.keys(image[`filtered`]).forEach((key) => {
+						image[`filtered`][key] = (!image[`filtered`][key].includes(`://`)) ? chrome.runtime.getURL(image[`filtered`][key]) : image[`filtered`][key];
+					});
+				} else if (Array.isArray(image[`filtered`])) {
+					image[`filtered`] = image[`filtered`].map((element) => {
+						return chrome.runtime.getURL(element);
+					});
+				};
+
 				return image[`filtered`];
 			})
 			.catch((error) => {
