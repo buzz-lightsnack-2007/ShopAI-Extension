4af1cc8e159a6686ffa2c21743a941079a4e208a
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-04-27 09:49:25 +0800

    modify icon command following a13702e0c207a44f4cc90b523b09fb1b7200fb47

--- scripts/GUI/entrypoints/icons.js
+++ scripts/GUI/entrypoints/icons.js
@@ -1,14 +1,21 @@
 import BrowserIcon from '/scripts/GUI/extensionIcon.js';
 import Image from '/scripts/mapping/image.js';
+import {session} from '/scripts/secretariat.js';
 
 class IconIndicator {
+     /* 
+     Indicate that the website is supported through icon change. 
+     */
      static async enable() {
-          // WIP. Make sure to determine if the product is bad / good
-          BrowserIcon.set(await Image.get('default'));
+          // Set default enabled icon. 
+          BrowserIcon.set({"Icon": await Image.get('default')});
      }
 
+     /* 
+     Indicate that the website isn't supported through icon change. 
+     */
      static async disable() {
-          BrowserIcon.set(await Image.get('disabled'));
+          BrowserIcon.setIcon({"Icon": await Image.get('disabled')});
      }
 }
 
