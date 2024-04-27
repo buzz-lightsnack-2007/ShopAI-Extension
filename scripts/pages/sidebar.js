42c310828b195421e530231b8bba277dc528554c
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-04-27 16:37:41 +0800

    prepare sidebar handler

--- scripts/pages/sidebar.js
+++ scripts/pages/sidebar.js
@@ -1,10 +1,10 @@
 import Sidebar from '../GUI/sidebar.js';
 import {read, write} from '../secretariat.js';
 
-export default class sidebar_handler {
+class sidebar_handler extends Page {
      constructor () {
-
-     };
+          super();
+     }
 
      async activate () {
           await read(`settings,behavior,autoRun`)
