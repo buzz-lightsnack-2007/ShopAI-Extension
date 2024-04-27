4c604581bd64d846e22f6028d002cf76fca2f10a
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-04-27 10:54:49 +0800

    add background listener for sidebar

--- scripts/GUI/entrypoints/sidebar.js
+++ scripts/GUI/entrypoints/sidebar.js
@@ -1,8 +1,21 @@
 import Sidebar from '/scripts/GUI/sidepanel.js'
 
 export default class ManagedSidebar {
-     constructor () {
-          // WIP
-          new Sidebar(`/pages/popup.htm`);
-     }
+	static manage() {
+		chrome.runtime.onMessage.addListener(
+			function(request, sender, sendResponse) {
+				if (request.action.includes("sidebar_open")) {
+					ManagedSidebar.enable();
+				}
+			}
+		);
+	}
+	
+	static enable() {
+		new Sidebar(`/pages/popup.htm`);
+	}
+
+	disable() {
+
+	}
 }
