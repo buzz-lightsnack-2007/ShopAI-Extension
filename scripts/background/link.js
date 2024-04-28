79cc72dfbaee0a1740bdbdbc6711ba0103eca63e
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-04-28 23:56:47 +0800

    add message receiving in the background

---
+++ scripts/background/link.js
@@ -0,0 +1,12 @@
+/* link.js
+Link the content scripts to the background scripts. 
+*/
+
+class link {
+     constructor () {
+          // Create a listener for events. 
+          chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+               
+          });
+     }
+}
