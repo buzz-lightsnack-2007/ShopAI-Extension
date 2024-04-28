8ce5ca71373daad4201e4c8b720c399c88ba8632
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-04-28 11:59:55 +0800

    anticipate tab updating away from a supported site

--- scripts/GUI/entrypoints/icons.js
+++ scripts/GUI/entrypoints/icons.js
@@ -4,32 +4,32 @@ import Tabs from '/scripts/GUI/tabs.js';
 import {session} from '/scripts/secretariat.js';
 
 class IconIndicator {
-     /*
-     Update the icon. 
-     
-     @param {string} state The state to update the icon to. 
-     */
-     static update(state = false) {
-          (Image.get(((state) ? `default` : 'disabled'))).then((ICON) => {
-               (Tabs.query(null, 0)).then((DATA) => {
-                    BrowserIcon.set({"Icon": ICON}, {"tabId": DATA.id});
-               });
-          })
-     }
+	/*
+	Update the icon. 
+	 
+	@param {string} state The state to update the icon to. 
+	*/
+	static update(state = false) {
+		(Image.get(((state) ? `default` : 'disabled'))).then((ICON) => {
+			(Tabs.query(null, 0)).then((DATA) => {
+				BrowserIcon.set({"Icon": ICON});
+			});
+		})
+	}
 
-     /* 
-     Indicate that the website is supported through icon change. 
-     */
-     static async enable() {
-          IconIndicator.update(true);
-     }
+	/* 
+	Indicate that the website is supported through icon change. 
+	*/
+	static async enable() {
+		IconIndicator.update(true);
+	}
 
-     /* 
-     Indicate that the website isn't supported through icon change. 
-     */
-     static async disable() {
-          // IconIndicator.update(false);
-     }
+	/* 
+	Indicate that the website isn't supported through icon change. 
+	*/
+	static async disable() {
+		IconIndicator.update(false);
+	}
 }
 
 export {IconIndicator as default};
