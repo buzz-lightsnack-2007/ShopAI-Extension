6816fddff2546e53adfee2219907b9261b3d6fe5
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-05-03 12:06:56 +0800

    unused background messaging removed

--- scripts/background/shopAI.js
+++ scripts/background/shopAI.js
@@ -4,8 +4,6 @@ Shop wisely with AI!
 
 import fc from './fc.js';
 import BackgroundCheck from "./background.check.js";
-import BackgroundMessaging from "./background.messaging.js";
 
 fc.run();
 BackgroundCheck.init();
-new BackgroundMessaging();
