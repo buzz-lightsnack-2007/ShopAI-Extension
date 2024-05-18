7eda4d82aa731f7f8dc93f6813f653d0df2669e9
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-05-18 09:53:30 +0800

    pass windowman generation settings

--- scripts/pages/page.js
+++ scripts/pages/page.js
@@ -5,19 +5,19 @@ Construct an internal page.
 import windowman from "/scripts/GUI/builder/windowman.js";
 
 export default class Page {
-     constructor () {
-          this.window = window;
-          this.window[`manager`] = new windowman();
-          
-          // Link the elements from this.window.manager to this.window for convenience later on. 
-          if ((this.window[`manager`])) {
-               this.window.manager.sync();
+	constructor (OPTIONS) {
+		this.window = window;
+		this.window[`manager`] = new windowman(OPTIONS);
+		
+		// Link the elements from this.window.manager to this.window for convenience later on. 
+		if ((this.window[`manager`])) {
+			this.window.manager.sync();
 
-               Object.assign(this.window, this.window[`manager`]);
-          }
+			Object.assign(this.window, this.window[`manager`]);
+		}
 
 		document.addEventListener("DOMContentLoaded", function () {
 			M.AutoInit();
 		});
-     }
+	}
 };
