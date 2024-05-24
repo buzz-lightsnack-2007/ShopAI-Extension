0d36039ba5caa6179531d68d956896bf4a7d1d00
buzz-lightsnack-2007 <73412182+buzz-lightsnack-2007@users.noreply.github.com>
2024-05-24 23:59:56 +0800

    add RegEx tester

---
+++ src/scripts/utils/RegExManager.js
@@ -0,0 +1,26 @@
+/*
+RegEx Manager
+Tests and manages regular expressions
+*/
+
+class RegExManager {
+	/*
+	Tests a regular expression.
+
+	@param {string} expression The regular expression to test.
+	@return {boolean} the state
+	*/
+	static test(expression) {
+		let RESULT = {};
+		RESULT[`state`] = false;
+		try {
+			console.log(expression);
+			RESULT[`expression`] = new RegExp(expression);
+			RESULT[`state`] = true;
+		} catch(err) {};
+
+		return (RESULT[`state`]);
+	};
+};
+
+export {RegExManager};
